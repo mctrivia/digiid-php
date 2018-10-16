9804d8082a3233e98a2424b598b65bfffe20959c
JMUATANTUM <JMUATANTUM@users.noreply.github.com>
2018-10-16 08:42:10 +0200

    Create AidTools.php

---
+++ AidTools.php
@@ -0,0 +1,27 @@
+
+<?php
+
+// Open Source Antum VOF 2018
+// Antum - Tools for AntumID - DigiID Platform System
+// Antum Optimized CODE for correct IP - 21-08-2018
+
+function get_client_ip() {
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+       $ipaddress = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+	}
+
+?>
