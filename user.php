338ed45e4c4cc51c75624ffdde49548b3e8c6215
JMUATANTUM <JMUATANTUM@users.noreply.github.com>
2018-10-16 08:58:35 +0200

    Update user.php

--- user.php
+++ user.php
@@ -15,23 +15,39 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 session_start(); ?>
+
 <!DOCTYPE html>
 <html>
 <head>
-    <title>DigiID Open Protocol - Demonstration site</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Open AntumID</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.css">
+    <link rel="stylesheet" href="assets/css/Login-Form-Clean.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
 </head>
 <body>
+    <div class="bounce animated login-clean">
+        <form method="post">
+            <h2 class="sr-only">Open AntumID - User Result</h2>
+            <center>
+            <img class="justify-content-center align-items-center align-content-center align-self-center visible" src="assets/img/digibytelogin.png" width="50%" height="50%" data-bs-hover-animate="pulse" style="width:105px;">
+            <br>Address that should be logged<br><br>
+            <small>  <?php echo $_SESSION['user_id']; ?> </small>
+           
+            <br>
+            <a href="index.php" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Return</a>
+        
 
-<div class="container">
-    <div class="tab-content">
-        <div class="tab-pane">
-            <div class="spacer40"></div>
-            <h3>Address that should be logged</h3>
-            <p><?php echo $_SESSION['user_id']; ?></p>
-            <div class="spacer40"></div>
-        </div>
+            </center>
+        </form>
     </div>
-</div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/bs-animation.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.js"></script>
 
 </body>
 </html>
